d98ee086119e58d2dad181b2aae242e9e9985646
matyiii <mezofimate@gmail.com>
2022-12-29 19:59:16 +0100

    setup server side rendering,  give tweets to Feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props{
+  tweets: Tweet[]
+}
+
+const Home  = ({tweets} : Props) => {
+  console.log(tweets);
   return (
     <div className="max-h-screen mx-auto overflow-hidden lg:max-w-6xl">
       <Head>
@@ -18,7 +25,7 @@ const Home: NextPage = () => {
         <Sidebar />
 
         {/* Feed-middle */}
-        <Feed />
+        <Feed tweets={tweets}/>
 
         {/* Widgets-right */}
         <Widgets />
@@ -28,3 +35,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+  return{
+    props:{
+      tweets,
+    }
+  }
+}
