240ebce78370aa78b78ccd54feb4076ab4775f80
matyiii <mezofimate@gmail.com>
2022-12-29 14:51:13 +0100

    added Feed component, responsive

---
+++ components/Feed.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {ArrowPathIcon} from '@heroicons/react/24/outline'
+import TweetBox from './TweetBox'
+
+function Feed() {
+  return (
+    <div className='col-span-7 lg:col-span-5 border-x'>
+        <div className='flex items-center justify-between'>
+            <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
+            <ArrowPathIcon className='w-8 h-8 mt-5 mr-5 transition-all duration-500 ease-out cursor-pointer text-twitter hover:rotate-180 active:scale-125'/>
+        </div>
+        {/* Tweetbox */}
+        <div>
+            <TweetBox />
+        </div>
+    </div>
+  )
+}
+
+export default Feed
