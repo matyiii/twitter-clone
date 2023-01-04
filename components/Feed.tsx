405980c8b8e0d601c36f134f6dd3fcdb02db89b9
matyiii <mezofimate@gmail.com>
2023-01-04 17:28:40 +0100

    feat: wokring Refresh button "refetch" tweets

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
-import {ArrowPathIcon} from '@heroicons/react/24/outline'
+import React, { useState } from 'react'
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
 import TweetBox from './TweetBox'
 import { Tweet } from '../typings'
 import TweetComponent from '../components/Tweet'
+import { fetchTweets } from '../utils/fetchTweets'
 
-interface Props{
+interface Props {
   tweets: Tweet[]
 }
 
-function Feed({tweets} : Props) {
+function Feed({ tweets: tweetsProp }: Props) {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProp)
+
+  const handleRefresh = async () => {
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+  }
+
   return (
     <div className='col-span-7 lg:col-span-5 border-x'>
-        <div className='flex items-center justify-between'>
-            <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-            <ArrowPathIcon className='w-8 h-8 mt-5 mr-5 transition-all duration-500 ease-out cursor-pointer text-twitter hover:rotate-180 active:scale-125'/>
-        </div>
-        {/* Tweetbox */}
-        <div>
-            <TweetBox />
-        </div>
+      <div className='flex items-center justify-between'>
+        <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
+        <ArrowPathIcon
+          onClick={handleRefresh}
+          className='w-8 h-8 mt-5 mr-5 transition-all duration-500 ease-out cursor-pointer text-twitter hover:rotate-180 active:scale-125' />
+      </div>
+      {/* Tweetbox */}
+      <div>
+        <TweetBox />
+      </div>
 
-        {/* Feed */}
-        <div>
-          {tweets?.map(tweet => (
-            <TweetComponent key={tweet._id} tweet={tweet}/>
-          ))}
-        </div>
+      {/* Feed */}
+      <div>
+        {tweets?.map(tweet => (
+          <TweetComponent key={tweet._id} tweet={tweet} />
+        ))}
+      </div>
     </div>
   )
 }
