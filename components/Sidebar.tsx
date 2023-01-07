87323f988488fdc8de6c7a6700df2730bd248392
matyiii <mezofimate@gmail.com>
2023-01-07 15:17:51 +0100

    feat: dynamic sign in/out based on session state

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import {HomeIcon,BellIcon, HashtagIcon, BookmarkIcon,
     EnvelopeIcon, QueueListIcon, UserIcon, EllipsisHorizontalCircleIcon} from '@heroicons/react/24/outline'
 import SidebarRow from './SidebarRow'
+import { signOut, signIn, useSession } from 'next-auth/react'
 
 function Sidebar(){
+    const {data: session} = useSession()
     return(
         <div className='flex flex-col items-center col-span-2 px-4 md:items-start'>
             <img className='w-10 h-10 m-3' src='images/twitter_logo_blue.png' alt=''/>
@@ -13,7 +15,7 @@ function Sidebar(){
             <SidebarRow Icon={EnvelopeIcon} title="Messages" />
             <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
             <SidebarRow Icon={QueueListIcon} title="Lists" />
-            <SidebarRow Icon={UserIcon} title="Sign In" />
+            <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? 'Sign Out' : 'Sign In'} />
             <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
         </div>
     )
