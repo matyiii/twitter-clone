af049cbe69b450dae16de3066e6ce231fc7b02a2
matyiii <mezofimate@gmail.com>
2023-01-16 19:22:11 +0100

    fix: scrollable feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -25,7 +25,7 @@ function Feed({ tweets: tweetsProp }: Props) {
   }
 
   return (
-    <div className='col-span-7 lg:col-span-5 border-x'>
+    <div className='max-h-screen col-span-7 overflow-scroll lg:col-span-5 border-x scrollbar-hide'>
       <div className='flex items-center justify-between'>
         <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
         <ArrowPathIcon
